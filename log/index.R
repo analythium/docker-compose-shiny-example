27928203c5e25dfb49db44f53a246e1880d17732
Peter Solymos <psolymos@gmail.com>
2021-06-15 20:52:32 -0600

    Sync with the post
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- log/index.R
+++ log/index.R
@@ -1,4 +1,5 @@
-HOST <- "159.203.22.73"
+HOST <- "159.203.22.73" # your IP or HOST name here
+
 system2("scp",
   c(
     sprintf(
@@ -8,19 +9,24 @@ system2("scp",
     "access.log"
   )
 )
-#scp root@$HOST:/root/docker-compose-shiny-example/log/access.log $PWD/log/access.log
+
 
 x <- lapply(
   readLines("access.log"),
   jsonlite::fromJSON)
 
+## timestamp
 ts <- as.POSIXct(sapply(x, "[[", "ts"), origin = "1970-01-01")
-req_uri <- sapply(x, function(z) z$request$uri)
+## URI
+uri <- sapply(x, function(z) z$request$uri)
+
+## find bare URIs
 s <- endsWith(req_uri, "/") & !endsWith(req_uri, "/websocket/")
 
 table(req_uri[s], as.Date(ts[s]))
-#              2021-06-15
-#  /                    7
-#  /covidapp/           1
-#  /hello/              3
-#  /pink/               4
+
+#              2021-06-15 2021-06-16
+#  /                    8          2
+#  /covidapp/           3          0
+#  /hello/              5          0
+#  /pink/               5          1
